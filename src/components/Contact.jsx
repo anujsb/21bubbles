2eb00c9123d53cf6f44852e00f81dc5b1a8b45c3
anuj <anujsbhuyar@gmail.com>
2024-01-31 17:54:59 +0530

    mobile view contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ const Contact = () => {
     <div className="h-screen">
       <div className="">
         <div>
-          <h1 className="m-10 text-black lg:text-9xl md:text-9xl text-left">
+          <h1 className="md:ml-10 lg:ml-10 sm:ml-0 text-black lg:text-8xl md:text-8xl sm:text-xl md:text-left lg:text-left sm:text-center">
             21bubbles.web<nav>@gmail.com</nav>
           </h1>
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -75,7 +75,7 @@ const Hero = () => {
             sometext
           </h1>
           <h1
-            className="mx-10 w-min title text-left lg:text-9xl md:text-9xl"
+            className="md:mx-10 lg:mx-10 sm:mx-0 w-min title text-left lg:text-9xl md:text-9xl"
             onMouseEnter={projectEnter}
             onMouseLeave={projectLeave}
           >
