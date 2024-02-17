5eb6a515e13768fd4f7dbd6be6f4bcd911e5bef7
anuj <anujsbhuyar@gmail.com>
2024-02-17 12:43:23 +0530

    contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,9 @@
 const Contact = () => {
   return (
-    <div className="h-auto">
-      <div className="">
+    <div className="h-auto z-[70]">
+      <div className="z-[70]">
         <div>
-          <h1 className="md:ml-10 lg:ml-10 sm:ml-0 my-20 text-black lg:text-8xl md:text-8xl sm:text-xl md:text-left lg:text-left sm:text-center "
+          <h1 className="md:ml-10 lg:ml-10 sm:ml-0 my-20 text-black lg:text-8xl md:text-8xl sm:text-xl md:text-left lg:text-left sm:text-center z-[70]"
           >
             21bubbles.web<nav>@gmail.com</nav>
           </h1>
